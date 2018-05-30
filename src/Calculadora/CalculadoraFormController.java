fd04068aa6e5029852c93a661f6730a2ab974964
fabricioJunior <fabriciojamescarneiro@gmail.com>
2018-05-30 15:09:59 -0300

    Filtros para erros de digitação
    Adicionado modificações e funções para contorna pequenos erros de digitação na calculadora
    
    Co-Authored-By: fabriciojunior <fabriciojunior@users.noreply.github.com>

--- src/Calculadora/CalculadoraFormController.java
+++ src/Calculadora/CalculadoraFormController.java
@@ -15,6 +15,7 @@ import Calculadora.operações.soma;
 import Calculadora.operações.divisão;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -46,7 +47,7 @@ public class CalculadoraFormController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         ArrayList<IOperações> ioperações = new ArrayList<IOperações>();
         ioperações.add(new soma());
         ioperações.add(new subtração());
@@ -56,7 +57,7 @@ public class CalculadoraFormController implements Initializable {
         ioperações.add(new fatorial());
         ioperações.add(new porcentagem());
         ioperações.add(new negativo());
-       
+
         operaçãoBox.getItems().addAll(ioperações);
         operaçãoBox.getSelectionModel().selectedItemProperty().addListener((obs, ant, nov) -> {
             IOperações selecionada = (IOperações) operaçãoBox.getSelectionModel().getSelectedItem();
@@ -67,11 +68,18 @@ public class CalculadoraFormController implements Initializable {
 
     public void calcularBtn_Clicked() {
 
+        num1Text.setText(Filtro(num1Text.getText()));
         IOperações selecionada = (IOperações) operaçãoBox.getSelectionModel().getSelectedItem();
         if (selecionada.binaria()) {
-            resultadoText.setText(selecionada.operação(num1Text.getText(), num2Text.getText()));
+            num2Text.setText(Filtro(num2Text.getText()));
+            if (ContemLetrasNum1() == false && ContemLetrasNum2() == false) {
+                resultadoText.setText(selecionada.operação(num1Text.getText(), num2Text.getText()));
+            }
+
         } else {
-            resultadoText.setText(selecionada.operação(num1Text.getText()));
+            if (!ContemLetrasNum1()) {
+                resultadoText.setText(selecionada.operação(num1Text.getText()));
+            }
         }
     }
 
@@ -91,6 +99,46 @@ public class CalculadoraFormController implements Initializable {
 
         if (key.getCode() == ENTER) {
             calcularBtn_Clicked();
+
         }
+    }
+
+    private String Filtro(String digitado) {
+        char[] palavra = digitado.toCharArray();
+        int qtd = palavra.length;
+        for (int x = 0; x < palavra.length; x++) {
+            if (palavra[x] == ',') {
+                palavra[x] = '.';
+            }
+        }
+        return String.valueOf(palavra);
+    }
+
+    private boolean ContemLetrasNum1() {
+        float valor;
+        if (num1Text.getText().length() != 0) {
+            try {
+                valor = Float.parseFloat(num1Text.getText());
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Digite um valor válido na primeira área de texto", "Informação", JOptionPane.INFORMATION_MESSAGE);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean ContemLetrasNum2() {
+        float valor;
+        if (num2Text.getText().length() != 0) {
+            try {
+                valor = Float.parseFloat(num2Text.getText());
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Digite um valor válido na segunda área de texto", "Informação", JOptionPane.INFORMATION_MESSAGE);
+
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/Calculadora/operações/fatorial.java
+++ src/Calculadora/operações/fatorial.java
@@ -7,12 +7,13 @@ package Calculadora.operações;
 
 import Calculadora.IOperações;
 import Calculadora.Operações;
+import java.text.DecimalFormat;
 
 /**
  *
  * @author Fabricio Junior
  */
-public class fatorial extends Operações implements IOperações{
+public class fatorial extends Operações implements IOperações {
 
     public fatorial() {
         binaria = false;
@@ -22,7 +23,7 @@ public class fatorial extends Operações implements IOperações{
     @Override
     public String operação(String num1) {
         long retorno = 1;
-        int n1 = Integer.parseInt(num1);
+        int n1 = (int) Float.parseFloat(num1);
         for (int x = 1; x <= n1; x++) {
             retorno = retorno * x;
         }
@@ -33,5 +34,5 @@ public class fatorial extends Operações implements IOperações{
     public String operação(String num1, String num2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-     
+
 }
